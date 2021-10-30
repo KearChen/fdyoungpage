4bd393156be061a49b6ae5b358b7168357e7196a
KearChen <70253413+KearChen@users.noreply.github.com>
2021-10-30 12:08:02 +0800

    Add files via upload

---
+++ rss.php
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0">
+<channel>
+<title><![CDATA[丰都青科盟]]></title> 
+<description><![CDATA[青年的学校]]></description>
+<link>https://www.fdyoung.cn/</link>
+<language>zh-cn</language>
+<generator>www.emlog.net</generator></channel>
+</rss>
